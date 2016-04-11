6c76821b1c9f5ac555f1a57dfcca3e42a7c52a59
Minister Face <ministerface@gmail.com>
2016-04-11 13:54:47 +0300

    config node for api

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
+
 var express = require('express');
 var bodyParser = require('body-parser')
 var path = require('path')
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
-app.get('/*', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = 8080;
+
+
+
+
+
+app.get('/api', function(req, res) {
+    res.json({ message: 'Zaebiss! welcome to our api!' });   
 });
 
-app.listen(8080);
-console.log("Node Server Listening on port 9090");
+
+app.use(express.static(__dirname + '/public'));
+// Here's the new code:
+app.use('*', function(req, res){
+  res.sendfile(__dirname + '/public/index.html');
+});
+
+
+app.listen(port);
+console.log("Node Server Listening on port 8080");
